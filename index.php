5bc18161c44f14e17488c4af66827418c9d9d0e8
Andrew R Emery <andrew@arevisual.com>
2020-06-28 22:59:53 -0400

    :tada: grid first pass

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+    <title>Homepage</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <main role=main>
+        <article class="bunny-comic-grid">
+            <section class="bunny-comic-hp-left">
+                <h1>1</h1>
+            </section>
+
+            <section class="bunny-comic-hp-right-top">
+                <h1>2</h1>
+            </section>
+
+            <section class="bunny-comic-hp-right-bottom">
+                <h1>3</h1>
+            </section>
+        </article>
+    </main>
+</body>
+
+</html>
