c289fd43396fe44a8ce6ac13b3180efb6d235634
Andrew R Emery <andrew@arevisual.com>
2020-06-30 17:02:14 -0400

    :sparkles: homepage grid

--- index.php
+++ index.php
@@ -3,21 +3,25 @@
 <head>
     <title>Homepage</title>
     <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.js"
+        integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="_js/page-flip.js"></script>
 </head>
 
 <body>
     <main role=main>
         <article class="bunny-comic-grid">
             <section class="bunny-comic-hp-left">
-                <h1>1</h1>
+
+                <h2>Visual Art</h2>
             </section>
 
             <section class="bunny-comic-hp-right-top">
-                <h1>2</h1>
+                <h2>About</h2>
             </section>
 
             <section class="bunny-comic-hp-right-bottom">
-                <h1>3</h1>
+                <h2>Writing</h2>
             </section>
         </article>
     </main>
